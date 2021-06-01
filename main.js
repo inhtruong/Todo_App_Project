e50f532f71c83df7215e26a387588c5460d0361a
inhtruong <duydat.dhsp@gmail.com>
2021-06-01 17:31:59 +0700

    Update lần 2 CS

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-const todos = document.querySelectorAll(".todo");
+const todo = document.querySelectorAll(".todo");
 const all_status = document.querySelectorAll(".status");
 let draggableTodo = null;
 
-todos.forEach((todo) => {
+todo.forEach((todo) => {
   todo.addEventListener("dragstart", dragStart);
   todo.addEventListener("dragend", dragEnd);
 });
@@ -53,74 +53,113 @@ function dragDrop() {
 }
 
 /* modal */
-const btns = document.querySelectorAll("[data-target-modal]");
+const btn = document.querySelectorAll("[data-target-modal]");
 const close_modals = document.querySelectorAll(".close-modal");
 const overlay = document.getElementById("overlay");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.querySelector(btn.dataset.targetModal).classList.add("active");
-    overlay.classList.add("active");
+function createModal() {
+  btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelector(btn.dataset.targetModal).classList.add("active");
+      overlay.classList.add("active");
+    });
   });
-});
-
-close_modals.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const modal = btn.closest(".modal");
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
+  
+  close_modals.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const modal = btn.closest(".modal");
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+    });
   });
-});
-
-window.onclick = (event) => {
-  if (event.target == overlay) {
-    const modals = document.querySelectorAll(".modal");
-    modals.forEach((modal) => modal.classList.remove("active"));
-    overlay.classList.remove("active");
-  }
-};
+  
+  window.onclick = (event) => {
+    if (event.target == overlay) {
+      const modals = document.querySelectorAll(".modal");
+      modals.forEach((modal) => modal.classList.remove("active"));
+      overlay.classList.remove("active");
+    }
+  };
+}
+createModal();
 
 /* create todo  */
 const todo_submit = document.getElementById("todo_submit");
-
 todo_submit.addEventListener("click", createTodo);
 
 function createTodo() {
   const todo_div = document.createElement("div");
   const input_val = document.getElementById("todo_input").value;
-  const txt = document.createTextNode(input_val);
+  if(isNullOrEmpty(input_val)) {
+    showMessage("This is required content!");
+    clear();
 
-  todo_div.appendChild(txt);
-  todo_div.classList.add("todo");
-  todo_div.setAttribute("draggable", "true");
+  } else {
+    const txt = document.createTextNode(input_val);
+  
+    todo_div.appendChild(txt);
+    todo_div.classList.add("todo");
+    todo_div.setAttribute("draggable", "true");
+  
+    /* create span */
+    const span = document.createElement("span");
+    const span_txt = document.createTextNode("\u00D7");
+    span.classList.add("close");
+    span.appendChild(span_txt);
+  
+    todo_div.appendChild(span);
+  
+    no_status.appendChild(todo_div);
+  
+    span.addEventListener("click", () => {
+      span.parentElement.style.display = "none";
+    });
+    //   console.log(todo_div);
+  
+    todo_div.addEventListener("dragstart", dragStart);
+    todo_div.addEventListener("dragend", dragEnd);
+  
+    document.getElementById("todo_input").value = "";
+    todo_form.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 
-  /* create span */
-  const span = document.createElement("span");
-  const span_txt = document.createTextNode("\u00D7");
-  span.classList.add("close");
-  span.appendChild(span_txt);
-
-  todo_div.appendChild(span);
-
-  no_status.appendChild(todo_div);
-
-  span.addEventListener("click", () => {
-    span.parentElement.style.display = "none";
-  });
-  //   console.log(todo_div);
-
-  todo_div.addEventListener("dragstart", dragStart);
-  todo_div.addEventListener("dragend", dragEnd);
-
-  document.getElementById("todo_input").value = "";
-  todo_form.classList.remove("active");
-  overlay.classList.remove("active");
 }
 
-const close_btns = document.querySelectorAll(".close");
+/* edit todo  */
 
-close_btns.forEach((btn) => {
+// const todo_info = document.getElementById("todo_info");
+// todo_info.addEventListener("click", editTodo);
+
+const close_btn = document.querySelectorAll(".close");
+close_btn.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.parentElement.style.display = "none";
   });
 });
+
+function isNullOrEmpty(str) {
+  return str == null || str.trim() == "";
+}
+
+function showMessage(msg){
+  let alert = document.getElementById('alert');
+  alert.classList.remove('d-none');
+  alert.innerHTML = msg;
+
+  autoCloseMessage();
+}
+function closeMessage(){
+  let alert = document.getElementById('alert');
+  alert.classList.add('d-none');
+}
+
+function autoCloseMessage(){
+  setInterval(() => {
+      closeMessage();
+  }, 5*1000); 
+}
+
+function clear() {
+  document.getElementById('todo_input').value = '';
+}
